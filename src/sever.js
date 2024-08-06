ffe23c41c22de140b41b81d6908289c0cdf1f52c
Silverss03 <nguyenhuumanhhp2003@gmail.com>
2024-08-06 17:39:12 +0700

    Lesson #30

--- src/sever.js
+++ src/sever.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
-const express = require('express') //common js?
+const express = require('express') //common js
 const path = require('path')
 const view_configuration = require('./config/ViewConfigEngine')
 const web_routers = require('./routes/web')
 const app = express() //app
 const port = process.env.PORT || 3001 //port
 const hostname = process.env.HOST_NAME
+const mysql = require('mysql2')
 
 //template view config
 view_configuration(app)
@@ -13,6 +14,22 @@ view_configuration(app)
 //route declare
 app.use('/', web_routers)
 
+//test connection
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port : process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+connection.query(
+    'SELECT * FROM Users',
+    function(err, results, fields) {
+        console.log("result",results) // results contains rows returned by server
+        //console.log("field",fields) // fields contains extra meta data about results, if available
+    }
+)
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
