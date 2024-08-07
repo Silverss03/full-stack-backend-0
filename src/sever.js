e8a77839d4005af4af6d4bacf59517e2ffda3ab1
Silverss03 <nguyenhuumanhhp2003@gmail.com>
2024-08-07 23:23:43 +0700

    Lesson #31

--- src/sever.js
+++ src/sever.js
@@ -6,22 +6,12 @@ const web_routers = require('./routes/web')
 const app = express() //app
 const port = process.env.PORT || 3001 //port
 const hostname = process.env.HOST_NAME
-const mysql = require('mysql2')
-
+const connection = require('./config/database')
 //template view config
 view_configuration(app)
 
 //route declare
 app.use('/', web_routers)
-
-//test connection
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port : process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
 
 connection.query(
     'SELECT * FROM Users',
