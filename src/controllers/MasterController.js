47958fbac30cfba8935227e88e0dae73275034b0
Silverss03 <nguyenhuumanhhp2003@gmail.com>
2024-08-08 18:52:25 +0700

    Lesson 34

--- src/controllers/MasterController.js
+++ src/controllers/MasterController.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const connection = require('../config/database')
 
 const getHomePage = (req, res) => {
-    res.send('Hello World yeah!')
+    connection.query(
+        'SELECT * FROM Users',
+        function(err, results, fields) {
+            console.log("result",results) // results contains rows returned by server
+            //console.log("field",fields) // fields contains extra meta data about results, if available
+            res.send(JSON.stringify(results))
+        }
+    )
 }
 
 const getNewPage = (req, res) => {
